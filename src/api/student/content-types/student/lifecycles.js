73d5b9dd45992c86ba33517bd55f8781421b8721
David Michael II <knowdavidmichael@gmail.com>
2022-06-01 19:55:59 +0100

    chore: add student lifecylce
    Automatically creates a User with the Users-Permissions plugin and adds an auto generated uid to the students model,

---
+++ src/api/student/content-types/student/lifecycles.js
@@ -0,0 +1,92 @@
+module.exports = {
+  async afterCreate(event)  {
+    const { result, params } = event;
+
+    const firstName = result.firstName.split('-')[0].toLowerCase();
+    const lastName = result.lastName.toString().toLowerCase();
+    const username = `${firstName}.${lastName}`;
+    const usernameAlt = `${firstName}.${lastName}${result.id}`;
+    const email = `${firstName}.${lastName}@stu.synk.io`;
+    const emailAlt = `${firstName}.${lastName}${result.id}@stu.synk.io`;
+    const date = new Date();
+    const password = `${firstName}${date.getFullYear()}?`;
+
+    function getStudentId(seq, year = '01', program = 'ECN') {
+      let getZero = '';
+      for (let i = 0; i < 5 - seq.toString().length; i += 1) {
+        getZero += '0';
+      }
+      return year + program + getZero + seq;
+    }
+
+    const oldUser = await strapi.db.query('plugin::users-permissions.user').findOne({
+      where: { email:email },
+    });
+
+    if(oldUser) {
+      try {
+        await strapi.plugins['users-permissions'].services.user.add({
+          blocked: false,
+          confirmed: true,
+          firstName: result.firstName,
+          lastName: result.lastName,
+          username: usernameAlt,
+          email: emailAlt,
+          password: password, //will be hashed automatically
+          provider: 'local', //provider
+          created_by: 1, //user admin id
+          updated_by: 1, //user admin id
+          role: 4 //role id
+        }).then(async (res)=>{
+          await strapi.db.query('api::student.student').update({
+            where: { id: result.id },
+            data: {
+              email: res.email,
+              uid: getStudentId(result.id),
+            }
+          });
+        });
+      } catch (e) {
+        console.log(e.errors)
+      }
+
+
+    }
+
+    try {
+      await strapi.plugins['users-permissions'].services.user.add({
+        blocked: false,
+        confirmed: true,
+        firstName: result.firstName,
+        lastName: result.lastName,
+        username: username,
+        email: email,
+        password: password, //will be hashed automatically
+        provider: 'local', //provider
+        created_by: 1, //user admin id
+        updated_by: 1, //user admin id
+        role: 4 //role id
+      }).then(async (res)=>{
+        await strapi.db.query('api::student.student').update({
+          where: { id: result.id },
+          data: {
+            email: res.email,
+            uid: getStudentId(result.id),
+          }
+        });
+      });
+    } catch (e) {
+      console.log(e);
+    }
+
+
+
+
+
+    // const entry = await strapi.db.query('plugin::users-permissions.user').findMany({});
+
+    // const entry = await strapi.plugins['users-permissions'].services.user.findMany();
+
+    // console.log(entry);
+  },
+};
