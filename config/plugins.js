cb3f80acf4fa0914a83c8b50383b7b754e3c24a2
David Michael II <knowdavidmichael@gmail.com>
2022-06-01 19:54:12 +0100

    config: add cloudinary plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,31 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "strapi-provider-upload-imagekit",  // Community providers need to have the full package name
+      provider: 'cloudinary',
       providerOptions: {
-        publicKey: env('IMAGEKIT_PUBLIC_KEY'),
-        privateKey: env('IMAGEKIT_PRIVATE_KEY'),
-        urlEndpoint: env('IMAGEKIT_URL_EP'),
-
-        // Optional
-        params: {
-          folder: env('IMAGEKIT_FOLDER')  // Defaults to "/" if value is not supplied
-        }
-      }
-    }
-  }
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // upload: {
+  //   config: {
+  //     provider: "strapi-provider-upload-imagekit",  // Community providers need to have the full package name
+  //     providerOptions: {
+  //       publicKey: env('IMAGEKIT_PUBLIC_KEY'),
+  //       privateKey: env('IMAGEKIT_PRIVATE_KEY'),
+  //       urlEndpoint: env('IMAGEKIT_URL_EP'),
+  //
+  //       // Optional
+  //       params: {
+  //         folder: env('IMAGEKIT_FOLDER')  // Defaults to "/" if value is not supplied
+  //       }
+  //     }
+  //   }
+  // }
 });
